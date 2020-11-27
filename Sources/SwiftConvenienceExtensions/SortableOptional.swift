ca8d0acfd5a746983fab84b69b34ead49667df7a
Nolaine Crusher <nolainecrusher@gmail.com>
2020-11-26 23:35:41 -0800

    added SortableOptional

---
+++ Sources/SwiftConvenienceExtensions/SortableOptional.swift
@@ -0,0 +1,66 @@
+//
+//  File.swift
+//
+//
+//  Created by Nolaine Crusher on 11/26/20.
+//
+
+import Foundation
+// books.sorted(by: { SortableOptional($0.seriesIndex) < SortableOptional($1.seriesIndex) })
+struct SortableOptional<T>: Comparable where T: Comparable {
+    var optional: T?
+    
+    static func == (lhs: Self, rhs: Self) -> Bool {
+        switch (lhs.optional, rhs.optional) {
+            case (.none, .none):
+                return true
+            case (.none, .some),
+                 (.some, .none):
+                return false
+            case (.some(let left), .some(let right)):
+                return left == right
+        }
+    }
+    
+    static func < (lhs: Self, rhs: Self) -> Bool {
+        switch (lhs.optional, rhs.optional) {
+            case (.none, .none):
+                return false
+            case (.none, .some):
+                return true
+            case (.some, .none):
+                return false
+            case (.some(let left), .some(let right)):
+                return left < right
+        }
+    }
+}
+
+struct ReversedSortableOptional<T>: Comparable where T: Comparable {
+    var optional: T?
+    
+    static func == (lhs: Self, rhs: Self) -> Bool {
+        switch (lhs.optional, rhs.optional) {
+            case (.none, .none):
+                return true
+            case (.none, .some),
+                 (.some, .none):
+                return false
+            case (.some(let left), .some(let right)):
+                return left == right
+        }
+    }
+    
+    static func < (lhs: Self, rhs: Self) -> Bool {
+        switch (lhs.optional, rhs.optional) {
+            case (.none, .none):
+                return false
+            case (.none, .some):
+                return false
+            case (.some, .none):
+                return true
+            case (.some(let left), .some(let right)):
+                return left < right
+        }
+    }
+}
